66ac73cae06bd1ce83dbdad3d4b71f7d5b61b387
digosilva <rrsilva1991@gmail.com>
2013-07-17 21:59:13 -0300

    Revert "teste"
    This reverts commit 45f0f5657dea076825ab689d842bdcb8ac57454a.

--- CasaMinha/src/com/ufpr/casaminha/controller/Inserir.java
+++ CasaMinha/src/com/ufpr/casaminha/controller/Inserir.java
@@ -1,12 +1,10 @@
 package com.ufpr.casaminha.controller;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
 import com.ufpr.casaminha.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 
 public class Inserir extends Activity {
 
@@ -14,10 +12,6 @@ public class Inserir extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inserir);
-		setContentView(R.layout.activity_inserir);
-		Spinner spinnerTipo = (Spinner) findViewById(R.id.tipo_spinner);
-		ArrayAdapter<CharSequence> tipoAdapter = ArrayAdapter.createFromResource(this,  R.array.tipo_arrays, android.R.layout.simple_spinner_item);
-		
 	}
 
 	@Override
