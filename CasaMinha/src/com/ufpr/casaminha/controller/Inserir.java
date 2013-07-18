ca0041e22dd9bc24848fb82cd06d28af26d31923
digosilva <rrsilva1991@gmail.com>
2013-07-18 20:32:12 -0300

    aula

--- CasaMinha/src/com/ufpr/casaminha/controller/Inserir.java
+++ CasaMinha/src/com/ufpr/casaminha/controller/Inserir.java
@@ -1,21 +1,28 @@
 package com.ufpr.casaminha.controller;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.ufpr.casaminha.R;
 import com.ufpr.casaminha.model.Imovel;
 
 public class Inserir extends Activity {
 
+	Imovel imovel;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -57,12 +64,32 @@ public class Inserir extends Activity {
 		String endereco = ((EditText) findViewById(R.id.endereco)).getText().toString();
 		Integer quartos = Integer.parseInt((String) ((Spinner) findViewById(R.id.qtos_spinner)).getSelectedItem());
 		Double valor = Double.parseDouble(((EditText) findViewById(R.id.valor_et)).getText().toString());
-		Imovel imovel = new Imovel(tipo, valor, vCondominio, endereco, quartos);
+		imovel = new Imovel(tipo, valor, vCondominio, endereco, quartos);
+		AsyncTask<Object, Object, Object> saveTask = new AsyncTask<Object, Object, Object>(){
+
+			@Override
+			protected Object doInBackground(Object... params) {
+				save();
+				return null;
+			}
+			
+		};
+		saveTask.execute((Object[]) null);
+		
+		
+		
+		
+	}
+	
+	public void save(){
 		DatabaseConnector dbConnector = new DatabaseConnector(Inserir.this);
 		dbConnector.insert(imovel);
-		Intent i = new Intent(this, MainActivity.class);
-		startActivity(i);
-		
+		Cursor result = dbConnector.findByEndereco(imovel);
+		CharSequence text = "Salvo "+result.getString(result.getColumnIndexOrThrow("tipo"))+
+				" no endereço "+ result.getString(result.getColumnIndexOrThrow("endereco"))+
+				"valor "+result.getString(result.getColumnIndexOrThrow("valor"));
+		Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+		toast.show();
 	}
 	
 	@Override
