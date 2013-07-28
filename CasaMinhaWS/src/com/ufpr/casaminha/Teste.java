5acf8d744333c871a538a121dbdc9c7f78eb8ff8
Thiago Frohlich <thi.frohlich@gmail.com>
2013-07-28 15:35:55 -0300

    Commit Projeto WS

---
+++ CasaMinhaWS/src/com/ufpr/casaminha/Teste.java
@@ -0,0 +1,53 @@
+package com.ufpr.casaminha;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Servlet implementation class Teste
+ */
+@WebServlet("/Teste")
+public class Teste extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Teste() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		PrintWriter out = response.getWriter();
+//		out.print("<HTML><HEAD><TITLE>Servlet</TITLE></HEAD><BODY>TESTE</BODY></HTML>");
+		HashMap<String, String> hm = new HashMap<>();
+		String login = request.getParameter("login");
+		hm.put("message", login);
+		
+		JSONObject json = JSONObject.fromObject(hm);
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.print(json);
+		out.flush();
+	}
+
+}
